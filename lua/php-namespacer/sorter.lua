dbdfcd246365d08159b2ebd6785c76e94aa46a92
carloshmfs <carloshmfs.dev@pm.me>
2024-07-23 00:13:43 -0300

    feat: sort the ast nodes by its length

--- lua/php-namespacer/sorter.lua
+++ lua/php-namespacer/sorter.lua
@@ -34,11 +34,23 @@ function Sorter:sort()
     local buffer = Sorter:get_text_buffer()
     local nodes = Sorter:get_use_declarations(buffer)
 
+    local ast_nodes = {}
     for id, node in nodes do
+        local start_row, start_col, end_row, end_col = node:range()
+        table.insert(ast_nodes, node)
+    end
+
+    table.sort(ast_nodes, function (a, b)
+        local _, _, _, a_end_col = a:range()
+        local _, _, _, b_end_col = b:range()
+
+        return a_end_col < b_end_col
+    end)
+
+    for i, node in ipairs(ast_nodes) do
         local node_type = node:type()
         local start_row, start_col, end_row, end_col = node:range()
-
-        print(string.format("Node: %s Line: %d Length: %d Content: %s", node_type, start_row + 1, end_col, vim.treesitter.get_node_text(node, buffer)))
+        print(string.format("Index: %d Node: %s Line: %d Length: %d Content: %s", i, node_type, start_row + 1, end_col, vim.treesitter.get_node_text(node, buffer)))
     end
 end
 
